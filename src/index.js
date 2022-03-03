3b2facdbd283fab645839795f1a512192cc0d4a5
Erin R <roulstonerin@gmail.com>
2022-03-03 03:02:02 -0500

    Simple pressure and flow rate input capture

--- src/index.js
+++ src/index.js
@@ -11,6 +11,65 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const initialValues = {
+    stomachPressure:"",
+    flowrate:""
+};
+
+class FrontPage extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {value:'', flowRate:''};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    const {value, flowRate} = this.state
+    event.preventDefault()
+    alert(`Stomach pressure submitted: ${value}\nFlow Rate submitted: ${flowRate}`);
+  }
+
+  handleChange(event){
+    // this.setState({value: event.target.value});
+    this.setState({[event.target.name]:event.target.value
+    })
+  }
+
+
+
+  render() {
+    return (
+   
+        <form onSubmit={this.handleSubmit}>
+             <br></br>
+          <div>
+        <label>Enter baby stomach pressure:</label>
+          <input name='value'  value={this.state.value} onChange={this.handleChange} />
+        </div>
+        <br></br>
+        <br></br>
+        <div>
+        <label>Enter feed flow rate:</label>
+          {/* <input type="text" value={this.state.flowrate} onChange={this.handleChange} /> */}
+          <input name='flowRate' value={this.state.flowrate} onChange={this.handleChange} />
+          <br></br>
+          <br></br>
+        <input type="submit" value="Submit" />
+        </div>
+      </form>
+     
+    );
+  }
+}
+
+// ========================================
+
+ReactDOM.render(
+  <FrontPage />,
+  document.getElementById('root')
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
